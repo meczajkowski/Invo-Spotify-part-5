5fe945c960bc488811a07d4897d21c83d608ac93
Michal <michal.erik.czajkowski@gmail.com>
2023-05-12 01:06:31 +0200

    add: styles for modal from lesson and custom close button

--- src/pages/home-page/components/user-feedback-modal/user-feedback-modal.js
+++ src/pages/home-page/components/user-feedback-modal/user-feedback-modal.js
@@ -3,5 +3,16 @@ import Swal from "sweetalert2";
 export const UserFeedbackModal = () => {
   Swal.fire({
     template: "#user-feedback-modal",
+    showConfirmButton: false,
+    width: "492px",
+    padding: 0,
+    customClass: {
+      container: "user-feedback-modal",
+    },
+    background: "var(--background-color)",
+    color: "var(--on-background-color)",
+    showCloseButton: true,
+    allowEnterKey: false,
+    closeButtonHtml: '<img src="/assets/icons/close-icon.svg" alt="X" />',
   });
 };
