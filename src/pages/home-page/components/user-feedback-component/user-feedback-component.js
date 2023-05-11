847f42c544330b2fa9e28bb8da7049a1e39793f3
Michal <michal.erik.czajkowski@gmail.com>
2023-05-12 00:33:44 +0200

    add: sending value of selected tile

--- src/pages/home-page/components/user-feedback-component/user-feedback-component.js
+++ src/pages/home-page/components/user-feedback-component/user-feedback-component.js
@@ -6,14 +6,25 @@ export const UserFeedbackComponent = () => {
     ".user-feedback-component__single-rate-tile"
   );
 
+  let currentRating;
+
   addListenersToAllRatesTiles();
 
   function addListenersToAllRatesTiles() {
     allRatesTiles.forEach((singleRateTile) => {
       singleRateTile.addEventListener("click", () => {
         singleRateTile.toggleAttribute("data-selected");
-
         disselectOtherRatings(singleRateTile);
+
+        if (singleRateTile.hasAttribute("data-selected")) {
+          currentRating = singleRateTile.getAttribute("data-rate");
+        } else {
+          currentRating = null;
+        }
+
+        console.log(`current rating: ${currentRating}`);
+
+        // afterRatingChanged(singleRateTile);
       });
     });
   }
